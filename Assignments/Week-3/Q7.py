be8cf507cec3642e773f0ab0c46da45d3220d2e5
Atm-06 <ece.190410045@silicon.ac.in>
2024-02-27 18:52:21 +0530

    Week-3-Asgn-1(q6-q10)

---
+++ Assignments/Week-3/Q7.py
@@ -0,0 +1,17 @@
+"""
+5 4 3 2 1
+4 3 2 1
+3 2 1
+2 1
+1
+"""
+
+
+def pattern(n: int):
+    for i in range(1, 6):
+        for j in range(6 - i, 0, -1):
+            print(j, end=" ")
+        print()
+
+
+pattern(5)
